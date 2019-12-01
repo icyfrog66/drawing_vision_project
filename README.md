3f726517b5f0206afa5f73d30f202727c4523d45
icyfrog66 <anthonylu8@gmail.com>
2019-11-30 23:24:22 -0800

    redone

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# drawing_vision_project
+# cvproject
+CS 455 Final Project
+
+
+How to use:
+
+python getdata.py 
+
+This command downloads data from the quickdraw dataset into the data folder.
+After that, each data/class subdirectory includes all the images of the specific class.
+
+python network1.py 
+
+This command will train on the neural network (currently the network only does a linear mapping)
---
+++ getdata.py
@@ -0,0 +1,20 @@
+from quickdraw import QuickDrawDataGroup
+import PIL
+import numpy
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+
+# Change these categories to download more categories
+desired_categories = ["aircraft carrier", "angel", "birthday cake", "car", "hand", "leg", "purse", "shoe"]
+for category in desired_categories:
+	samples = QuickDrawDataGroup(category)
+	directory_name = "data/" + str(category)
+	if not os.path.exists(directory_name):
+		os.makedirs(directory_name)
+	for i in range(samples.drawing_count):
+		drawing_i = samples.get_drawing(i)
+		drawing_i.image.save(directory_name + "/pic" + str(i) + ".png")
+
+# Quickdraw api
+# https://quickdraw.readthedocs.io/en/latest/api.html
